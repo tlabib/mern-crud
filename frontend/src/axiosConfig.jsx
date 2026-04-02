19197878e63b307bc9cffe9fb3b3bfb0284847f9
Tahmidul Labib <tahmidul.labib@g.bracu.ac.bd>
2026-04-02 22:12:21 +0600

    added my public ip to axiosConfig.js

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
   //baseURL: 'http://3.26.96.188:5001', // live
+  //baseURL: 'http://3.106.242.155:5001', // live my public ip
   headers: { 'Content-Type': 'application/json' },
 });
 
